840ce8a5eabd0676bc82fab7b4330bd92bf3576d
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2022-08-19 00:57:42 +0100

    working on button component of the demo app

---
+++ demo-app/components/PrimaryButtons.js
@@ -0,0 +1,51 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("I too cool");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    overflow: "hidden",
+    margin: 4,
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.3,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
